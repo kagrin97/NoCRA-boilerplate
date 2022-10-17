161ca725da073e1bc1f3bacc2caf44608e3d32b0
MinGyu-Kang <kagrin97@gmail.com>
2022-10-17 17:03:13 +0900

    fix : eslint 버그 수정 및 webpack설정 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   printWidth: 100,
   singleQuote: true, // "" => ''
-  arrowParens: "avoid", // arrow function parameter가 하나일 경우 괄호 생략
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const App = () => (
   <>
-    <h1>React without CRsdfA...</h1>
+    <h1>React without CRA...</h1>
   </>
 );
 
